6189b125fa3cb7df248bed4dc3809f11fae1da66
thngph <ngophuocthinh28@gmail.com>
2024-10-12 20:47:59 +0700

    💥 update(web): Rename file and remove fragment

--- apps/web/src/pages/NotFoundPage.tsx
+++ apps/web/src/pages/NotFoundPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { Trans, useTranslation } from 'react-i18next';
 
@@ -12,27 +11,25 @@ export const NotFoundPage = () => {
   usePageTitle(t('pageTitle.notFound'));
 
   return (
-    <React.Fragment>
-      <div className="grid flex content-center justify-center align-center space-y-6 h-[80vh] md:h-screen">
-        <div className="flex justify-center">
-          <img src={getImgUrl('not_found.svg')} className="max-w-xs md:max-w-md" alt="Not Found" />
-        </div>
-        <div className="flex flex-col text-center max-w-screen-sm lg:max-w-screen-md px-4">
-          <h1 className="py-6 text-2xl md:text-6xl font-medium">{t('notFound.title')}</h1>
-          <p className="text-sm md:text-lg">
-            {t('notFound.description')}
-            <br />
-            <Trans
-              i18nKey="notFound.backHome"
-              components={{
-                toHome: <Link className="underline" to={PATH.HOME} />,
-                toContact: <Link className="underline" to={PATH.HOME} /> // TODO: Change to contact page
-              }}
-            />
-          </p>
-        </div>
+    <div className="grid flex content-center justify-center align-center space-y-6 h-[80vh] md:h-screen">
+      <div className="flex justify-center">
+        <img src={getImgUrl('not-found.svg')} className="max-w-xs md:max-w-md" alt="Not Found" />
       </div>
-    </React.Fragment>
+      <div className="flex flex-col text-center max-w-screen-sm lg:max-w-screen-md px-4">
+        <h1 className="py-6 text-2xl md:text-6xl font-medium">{t('notFound.title')}</h1>
+        <p className="text-sm md:text-lg">
+          {t('notFound.description')}
+          <br />
+          <Trans
+            i18nKey="notFound.backHome"
+            components={{
+              toHome: <Link className="underline" to={PATH.HOME} />,
+              toContact: <Link className="underline" to={PATH.HOME} /> // TODO: Change to contact page
+            }}
+          />
+        </p>
+      </div>
+    </div>
   );
 };
 
