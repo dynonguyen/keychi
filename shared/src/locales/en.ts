6aff3d8ca9ea5cf615087e61f3a839c3706972a1
Ngô Phước Thịnh <67597758+thngph@users.noreply.github.com>
2024-10-12 20:53:58 +0700

    ✨ feat(web): Not found page (#1)

--- shared/src/locales/en.ts
+++ shared/src/locales/en.ts
@@ -2,6 +2,11 @@ const en = {
   pageTitle: {
     home: 'Home page',
     notFound: 'Page not found'
+  },
+  notFound: {
+    title: 'Oops!',
+    description: 'We can\'t seem to find the page you\'re looking for.',
+    backHome: 'Return to our <toHome>home page</toHome> or <toContact>contact us</toContact>.'
   }
 } as const;
 
