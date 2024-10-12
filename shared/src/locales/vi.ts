754c8344869c7583eda23e1d3cdeb69139508f92
thngph <ngophuocthinh28@gmail.com>
2024-10-12 19:53:36 +0700

    ✨ feat(web): Not found page

--- shared/src/locales/vi.ts
+++ shared/src/locales/vi.ts
@@ -2,6 +2,11 @@ const vi = {
   pageTitle: {
     home: 'Trang chủ',
     notFound: 'Không tìm thấy trang'
+  },
+  notFound: {
+    title: 'Không tìm thấy trang.',
+    description: 'Trang này không tồn tại hoặc đã bị xóa!',
+		backHome: 'Quay về <toHome>trang chủ</toHome> hoặc <toContact>liên hệ với chúng tôi</toContact>.'
   }
 } as const;
 
