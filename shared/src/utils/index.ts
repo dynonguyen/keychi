80f43cd40231d49c48031e5493f856efc9df864f
Dyno Nguyen <tuannguyentn2504@gmail.com>
2024-11-27 23:21:40 +0700

    ✨ feat(shared): Add password generator tool

--- shared/src/utils/index.ts
+++ shared/src/utils/index.ts
@@ -4,5 +4,6 @@ export * from './get-env';
 export * from './helper';
 export * from './hof';
 export * from './mapping';
+export * from './password-tools';
 export * from './totp';
 export * from './validation';
