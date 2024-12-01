1931636e2ed7b22b73b75d2d72c0246b99f63594
thngph <ngophuocthinh28@gmail.com>
2024-12-01 19:09:32 +0700

    🧹 clean(api): Clean

--- api/internal/user/delivery/rest/update_user_preferences.go
+++ api/internal/user/delivery/rest/update_user_preferences.go
@@ -24,8 +24,7 @@ func HandleUpdateUserPreferences(s *infra.PgsqlStorage) echo.HandlerFunc {
 			return c.JSON(http.StatusBadRequest, common.NewBadRequestError(err, common.CodeBadRequestError))
 		}
 
-		err := uc.UpdateUserPreferences(c.Request().Context(), id, &input)
-		if err != nil {
+		if err := uc.UpdateUserPreferences(c.Request().Context(), id, &input); err != nil {
 			return c.JSON(common.GetAppErrorStatus(err, http.StatusInternalServerError), err)
 		}
 		return c.JSON(http.StatusOK, common.NewOkResponse[interface{}](nil))
