8ef98a0a1607e517211943b11e2f1115c692dab1
thngph <ngophuocthinh28@gmail.com>
2025-01-16 00:45:52 +0700

    🐞 fix(web): Get theme for toast if not pref not set

--- apps/web/src/components/ThemeWatcher.tsx
+++ apps/web/src/components/ThemeWatcher.tsx
@@ -1,18 +1,8 @@
-import { DEFAULT } from '@keychi/shared/constants';
 import { ThemeMode } from '@keychi/shared/types';
 import React from 'react';
 import { LS_KEY } from '../constants/key';
+import { getActualTheme, getLSTheme } from '../hooks/useTheme';
 import { useProfileStore } from '../stores/profile';
-
-const getActualTheme = (theme: ThemeMode): ThemeMode => {
-  if (theme !== ThemeMode.System) return theme;
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? ThemeMode.Dark : ThemeMode.Light;
-};
-
-const getLSTheme = (): ThemeMode => {
-  const savedTheme = localStorage.getItem(LS_KEY.THEME) as ThemeMode | undefined;
-  return savedTheme && Object.values(ThemeMode).includes(savedTheme) ? savedTheme : DEFAULT.USER_THEME;
-};
 
 const ThemeWatcher = () => {
   const theme = useProfileStore((state) => state?.preferences?.theme);
--- apps/web/src/hooks/useTheme.tsx
+++ apps/web/src/hooks/useTheme.tsx
@@ -1,22 +1,38 @@
-import { ENDPOINT } from '@keychi/shared/constants';
+import { DEFAULT, ENDPOINT } from '@keychi/shared/constants';
 import { PreferencesReqDto, PreferencesType, ThemeMode } from '@keychi/shared/types';
 import { getErrorMessage } from '@keychi/shared/utils';
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import { ApiResponse } from 'shared/src/types/common.type';
+import { LS_KEY } from '../constants/key';
 import { mutation } from '../libs/query-client';
 import { useAuthStore } from '../stores/auth';
 import { useProfileStore } from '../stores/profile';
 
 const updateTheme = mutation<ApiResponse, PreferencesReqDto>(ENDPOINT.PATCH_PREFERENCES, undefined, 'patch');
 
+export const getActualTheme = (theme: ThemeMode): ThemeMode => {
+  if (theme !== ThemeMode.System) return theme;
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? ThemeMode.Dark : ThemeMode.Light;
+};
+
+export const getLSTheme = (): ThemeMode => {
+  const savedTheme = localStorage.getItem(LS_KEY.THEME) as ThemeMode | undefined;
+  return savedTheme && Object.values(ThemeMode).includes(savedTheme) ? savedTheme : DEFAULT.USER_THEME;
+};
+
 export const useTheme = () => {
   const themeMutation = useMutation({ mutationFn: updateTheme });
 
-  const isDark = useProfileStore((state) => state?.preferences?.theme === ThemeMode.Dark);
+  const themePreference = useProfileStore((state) => state?.preferences?.theme);
+  const isDark = getActualTheme(themePreference || getLSTheme()) === ThemeMode.Dark;
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
   const setTheme = async (mode: ThemeMode) => {
+    if (themePreference === mode) {
+      return;
+    }
+
     if (isAuthenticated) {
       const [error] = await themeMutation.mutateAsync({
         type: PreferencesType.UI,
